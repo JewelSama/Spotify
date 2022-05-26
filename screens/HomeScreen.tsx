ab17203bfac469e6be88e02bcdd960aad96b73f6
JewelKahz <67759825+JewelKahz@users.noreply.github.com>
2022-05-26 12:48:05 +0100

    album component

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,15 +1,23 @@
 import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
 
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
+import Album from '../components/Album';
+
+
+const album ={
+  id: '1',
+  imageUri: 'https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg',
+  artistsHeadline: 'Taylor Swift, Kygo Objective C, Avicii'
+}
+
+
 export default function HomeScreen({ navigation }: RootTabScreenProps<'HomeScreen'>) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Home</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+        <Album album={album} />
     </View>
   );
 }
